744d95349981b7453c8f9101233fe5a8956c55f6
Nicholas De Kock <u17198021@tuks.co.za>
2022-03-23 15:53:17 +0200

    updated dependancies

--- autoesda/autoesda.py
+++ autoesda/autoesda.py
@@ -1,1 +1,13 @@
 """Main module."""
+
+import geopandas as gpd
+import pysal as ps
+import matplotlib.pyplot as plt
+import libpysal as lps
+from esda.moran import (Moran, Moran_Local)
+from splot.esda import (moran_scatterplot, lisa_cluster, plot_moran_simulation)
+from matplotlib.offsetbox import AnchoredText
+import seaborn as sns
+from seaborn import heatmap
+import io
+import base64
