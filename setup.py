4d5edb715176c93e6c78c9f75872683145a8a90b
Nic De Kock <ndekock@esri-southafrica.com>
2024-03-13 14:02:29 +0200

    Bump version: 0.0.6 → 1.0.0

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/NicholasDeKock/autoesda',
-    version='0.0.6',
+    version='1.0.0',
     zip_safe=False,
 )
